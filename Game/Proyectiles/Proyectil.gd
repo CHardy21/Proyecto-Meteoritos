1a26706697982787c1dfb3302b150aa41471889f
Christian <christianrh2000@hotmail.com>
2023-10-30 12:24:20 -0300

    pulido 1

--- Game/Proyectiles/Proyectil.gd
+++ Game/Proyectiles/Proyectil.gd
@@ -4,7 +4,7 @@ extends Area2D
 var velocidad:Vector2 = Vector2.ZERO
 var danio:float = 1.0
 
-func crear(pos:Vector2, dir:float, vel:float, danio_p:int)-> void:
+func crear(pos:Vector2, dir:float, vel:float, _danio_p:int)-> void:
 	position = pos
 	rotation = dir
 	velocidad = Vector2(vel,0).rotated(dir)
