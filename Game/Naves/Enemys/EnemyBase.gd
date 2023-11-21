234858e66843e7a8a57c7a48017f536bf1aa5d67
Christian <christianrh2000@hotmail.com>
2023-11-21 11:42:21 -0300

    persiguiendo al Payer ok

--- Game/Naves/Enemys/EnemyBase.gd
+++ Game/Naves/Enemys/EnemyBase.gd
@@ -4,6 +4,8 @@ extends NaveBase
 
 # Atributos
 var player_objetivo:Players = null
+var dir_player:Vector2
+
 
 # Metodos
 func _ready() -> void:
@@ -12,10 +14,10 @@ func _ready() -> void:
 	# Temporal
 	#canion.set_esta_disparando(true)
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	rotar_hacia_player()
 
-# Señales
+# Señales internas
 func _on_body_entered(body: Node) -> void:
 	._on_body_entered(body)
 	if body is Players:
@@ -29,7 +31,7 @@ func _on_nave_destruida(nave:NaveBase,_posicion, _explosiones)->void:
 
 func rotar_hacia_player()->void:
 	if player_objetivo:
-		var dir_player:Vector2 = player_objetivo.global_position - global_position
+		dir_player = player_objetivo.global_position - global_position
 		rotation = dir_player.angle()
 	
 	
--- Game/Naves/Enemys/EnemyInterceptor.gd
+++ Game/Naves/Enemys/EnemyInterceptor.gd
@@ -5,34 +5,64 @@ extends EnemyBase
 # Enums
 enum ESTADO_IA { IDLE, ATACANDOQ, ATACANDOP, PERSECUCION }
 
+# ATRIBUTOS EXPORT
+export var potencia_max:float = 800.0
+
 # ATRIBUTOS
 var estado_ia_actual:int = ESTADO_IA.IDLE
+var potencia_actual:float = 0.0
+
+
+
+
+# Metodos
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	linear_velocity += dir_player.normalized() * potencia_actual * state.get_step()
+	
+	linear_velocity.x = clamp(linear_velocity.x, -potencia_max, potencia_max)
+	linear_velocity.y = clamp(linear_velocity.y, -potencia_max, potencia_max)
+#	if potencia_actual == potencia_max:
+#		print("linear_velocity.x: ", linear_velocity.x)
+#		print("linear_velocity.y: ", linear_velocity.y)
+#		print(potencia_actual)
+#		print(potencia_max)
 
 # Metodos Custom
+# FSM (Maquina de Estados Finitos)
 func controlador_estados_ia(nuevo_estado:int) -> void:
 	match nuevo_estado:
 		ESTADO_IA.IDLE:
 			canion.set_esta_disparando(false)
+			potencia_actual = 0.0
 		ESTADO_IA.ATACANDOQ:
 			canion.set_esta_disparando(true)
+			potencia_actual = 0.0
 		ESTADO_IA.ATACANDOP:
 			canion.set_esta_disparando(true)
+			potencia_actual = potencia_max
 		ESTADO_IA.PERSECUCION:
 			canion.set_esta_disparando(false)
+			potencia_actual = potencia_max
 		_:
 			print("ERROR de Estados.")
-	
+
 	estado_ia_actual = nuevo_estado
 
 # Señales Internas
-func _on_AreaDisparo_body_entered(body):
+func _on_AreaDisparo_body_entered(body:Node):
 	controlador_estados_ia(ESTADO_IA.ATACANDOP)
+	print(estado_ia_actual)
 
-func _on_AreaDisparo_body_exited(body):
+func _on_AreaDisparo_body_exited(body:Node):
 	controlador_estados_ia(ESTADO_IA.PERSECUCION)
+	print(estado_ia_actual)
 
-func _on_AreaDeteccion_body_entered(body):
+func _on_AreaDeteccion_body_entered(body:Node):
 	controlador_estados_ia(ESTADO_IA.ATACANDOQ)
+	print(estado_ia_actual)
 
-func _on_AreaDeteccion_body_exited(body):
+func _on_AreaDeteccion_body_exited(body:Node):
 	controlador_estados_ia(ESTADO_IA.ATACANDOP)
+	print(estado_ia_actual)
+
+
